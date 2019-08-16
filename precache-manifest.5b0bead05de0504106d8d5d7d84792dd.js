c1539a86c32db39b0e048946a8e6bd2a173408af
이규종 <kyujong93@naver.com>
2019-08-16 17:23:04 +0900

    Updates

---
+++ precache-manifest.5b0bead05de0504106d8d5d7d84792dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37731831aa2c9b7e42bd29252dac2adb",
+    "url": "/kyuflix/index.html"
+  },
+  {
+    "revision": "69081786e7272f97cc6b",
+    "url": "/kyuflix/static/js/2.e6234b12.chunk.js"
+  },
+  {
+    "revision": "afb4ae4b4be0e29abefc",
+    "url": "/kyuflix/static/js/main.6c5de97c.chunk.js"
+  },
+  {
+    "revision": "14e57000156947ceb339",
+    "url": "/kyuflix/static/js/runtime~main.9ad56ba5.js"
+  },
+  {
+    "revision": "19d65de1ea581c809cfcc7ea8be8a555",
+    "url": "/kyuflix/static/media/noImage.19d65de1.png"
+  }
+]);

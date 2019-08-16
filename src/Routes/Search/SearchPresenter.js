f1af49361f27ae4ec810e18735fb480cd9963883
이규종 <kyujong93@naver.com>
2019-08-16 17:36:47 +0900

    deploy gh-pages

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -8,7 +8,7 @@ import Poster from '../../Components/Poster';
 import Helmet from 'react-helmet';
 
 const Container = styled.div`
-  padding: 0 20px;
+  padding: 20px;
 `;
 
 const Form = styled.form`
